798bd6a0f8cc6655ed59b8313e28e690a214f8c6
Jonathan Bees <jonathan.bees1@gmail.com>
2014-06-17 11:10:21 -0400

    Now checks for all characters between ASCII values 32 and 126 (space and tilde) Removed the vestiges of the old code Updated the version string

--- src/DigitCount.java
+++ src/DigitCount.java
@@ -1,31 +1,17 @@
 /**
- * Version 1.5 (Now checks the last 10 characters of a password)
+ * Version 1.6 (Now checks for all ASCII characters between values 32 and 126, space and tilde)
  * Created by Jonathan Bees on 6/9/2014
- * Updated by Jonathan Bees on 6/16/2014
+ * Updated by Jonathan Bees on 6/17/2014
  */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class DigitCount {
     //initializes all of the variables
-
-    public int conditionOne; //990 total
-    public int conditionTwo; //983 total
-    public int conditionThree; //3062 total
-    public int conditionFour; //2054 total
-    public int conditionFive; //987 total
-    public int conditionSix; //4239 total
-    public int conditionSeven; //15000 total
-    public int conditionEight; //15000 total
-    public int conditionNine; //1394 total
-    public int conditionTen; //564 total
-    public int conditionEleven; //1155 total
-
 
     public int linesProcessed = 0;
 
@@ -43,7 +29,6 @@ public class DigitCount {
 
     public void run(String filePath) throws Exception {
         stringReader(filePath);
-        //stringMatcher("C:\\Users\\jonat_000\\Documents\\CUPS Stuff\\fullpw_pw.txt", "C:\\Users\\jonat_000\\Documents\\CUPS Stuff\\fullpw_jtr.txt", "C:\\Users\\jonat_000\\Documents\\CUPS Stuff\\fullpw_cond.txt");
     }
 
     public void stringReader(String filePath) throws Exception {
@@ -65,7 +50,7 @@ public class DigitCount {
         // print out counts
         char curChar;
         TreeMap<Integer, Integer> curCounts;
-        for (int i = (int) '0'; i <= (int) '9'; i++) {
+        for (int i = 32; i <= 126; i++) {
             curChar = (char) i;
             curCounts = (TreeMap<Integer, Integer>) charCounts.get(curChar);
             System.out.format("'%s':", curChar); // make it pretty
@@ -83,7 +68,7 @@ public class DigitCount {
         // print out percentages
         double pct;
         int curCount;
-        for (int i = (int) '0'; i <= (int) '9'; i++) {
+        for (int i = 32; i <= 126; i++) {
             curChar = (char) i;
             curCounts = (TreeMap<Integer, Integer>) charCounts.get(curChar);
             System.out.format("'%s':", curChar); // make it pretty
@@ -111,7 +96,7 @@ public class DigitCount {
         String curLineReverse = new StringBuilder(curLine).reverse().toString();
 
         for (int i = 0; i < length; i++) {
-            curChar = curLineReverse.charAt(i);
+            curChar = curLine.charAt(i);
             curCounts = (TreeMap<Integer, Integer>) charCounts.get(curChar);
 
             if (curCounts == null) {
@@ -133,81 +118,4 @@ public class DigitCount {
         linesProcessed++;
     }
 
-
-    /*public void stringMatcher (String passwordPath, String crackerPath, String conditionPath) throws Exception
-    {
-        BufferedReader passBr = new BufferedReader(new FileReader(passwordPath));
-        BufferedReader crackBr = new BufferedReader(new FileReader(crackerPath));
-        BufferedReader condBr = new BufferedReader(new FileReader(conditionPath));
-
-        while (true){
-            String crackLine = crackBr.readLine();
-            String passLine = passBr.readLine();
-            String condLine = condBr.readLine();
-
-            if (passLine == null || crackLine == null || condLine == null)
-                break;
-
-            //defines all of the strings indexOf should be looking for
-            String notCracked = "-5";
-            String condOne = "3class12";
-            String condTwo = "3class16";
-            String condThree = "basic8";
-            String condFour = "basic16";
-            String condFive = "basic20";
-            String condSix = "comp8";
-            String condSeven = "rockyou";
-            String condEight = "yahoo";
-            String condNine = "metersStandard";
-            String condTen = "metersStringent";
-            String condEleven = "blacklist";
-
-            //checks if a string has been cracked
-            int crackCheck = crackLine.indexOf(notCracked);
-            //checks which condition a password was created under
-            int condOneMatch = condLine.indexOf(condOne);
-            int condTwoMatch = condLine.indexOf(condTwo);
-            int condThreeMatch = condLine.indexOf(condThree);
-            int condFourMatch = condLine.indexOf(condFour);
-            int condFiveMatch = condLine.indexOf(condFive);
-            int condSixMatch = condLine.indexOf(condSix);
-            int condSevenMatch = condLine.indexOf(condSeven);
-            int condEightMatch = condLine.indexOf(condEight);
-            int condNineMatch = condLine.indexOf(condNine);
-            int condTenMatch = condLine.indexOf(condTen);
-            int condElevenMatch = condLine.indexOf(condEleven);
-
-            //does the actual check for if a password was cracked or not, and, if so, checks what condition it was created under
-            if (crackCheck != 0){
-                processLine(passLine);
-                if (condOneMatch != -1)
-                    conditionOne++;
-                if (condTwoMatch != -1)
-                    conditionTwo++;
-                if (condThreeMatch != -1)
-                    conditionThree++;
-                if (condFourMatch != -1)
-                    conditionFour++;
-                if (condFiveMatch != -1)
-                    conditionFive++;
-                if (condSixMatch != -1)
-                    conditionSix++;
-                if (condSevenMatch != -1)
-                    conditionSeven++;
-                if (condEightMatch != -1)
-                    conditionEight++;
-                if (condNineMatch != -1)
-                    conditionNine++;
-                if (condTenMatch != -1)
-                    conditionTen++;
-                if (condElevenMatch != -1)
-                    conditionEleven++;
-
-                linesMatched++;
-            }
-        }
-        passBr.close();
-        crackBr.close();
-        condBr.close();
-    }*/
 }
