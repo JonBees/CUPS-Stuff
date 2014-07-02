24d43bddcb06fc8abcca0db1df3c5f39b263275d
Jonathan Bees <jonathan.bees1@gmail.com>
2014-07-02 14:22:55 -0400

    The password strength checker is now semi-functional. (It works for the last5 analysis)

--- src/CharStats.java
+++ src/CharStats.java
@@ -1,12 +1,13 @@
 /**
- * Version 1.10 (Now prints both first5 and last5 automagically instead of needing a command line argument and separate runs.)
+ * Version 1.11 (Now outputs scores for all given passwords.)
  * Created by Jonathan Bees on 6/9/2014
- * Updated by Jonathan Bees on 7/1/2014
+ * Updated by Jonathan Bees on 7/2/2014
  */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -18,13 +19,16 @@ public class CharStats {
 
     HashMap<Character, SortedMap<Integer, Integer>> charCounts;
 
+    static String filePath;
     String csvFilePath;
     CSVFormat csvFormat = CSVFormat.EXCEL.withCommentStart(' ');
     CSVPrinter csvPrinter;
 
-    int status = 0; //Status of 0 is the equivalent of firstHalfDone=false. Status of 1 is the equivalent of firstHalfDone=true. Status of 2 is the password security analysis.
-
     int numLines;
+
+    ArrayList passwordScore = new ArrayList();
+
+    int status = 1; //Status of 1 is first5 letter frequency analysis. Status of 2 is first5 password score analysis. Status of 3 is last5 letter frequency analysis. Status of 4 is last5 password score analysis.
 
     public CharStats() {
         charCounts = new HashMap<>();
@@ -33,44 +37,67 @@ public class CharStats {
     public static void main(String args[]) throws Exception {
         //checks for command line arguments, then creates an instance of the object and starts the run method
         CharStats counter = new CharStats();
-        counter.run(args[0]);
+        filePath = args[0];
+        counter.run();
     }
 
-    public void run(String filePath) throws Exception {
-        stringReader(filePath);
-        output(filePath);
+    public void run() throws Exception {
+        stringReader();
+        output();
     }
 
-    public void stringReader(String filePath) throws Exception {
+    public void stringReader() throws Exception {
+        System.out.println("Status: " + status);
         System.out.println("Started reading the file.");
 
         //buffers the lines and hands off the processing for each line to the processLine method
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String line;
-        numLines = 0;
+
+        int curLineNum = 1;
+
         while ((line = br.readLine()) != null) {
-            if(status == 1 || status == 2) {
-                processLine(line);
+            if(status == 1 || status == 3) {
+                processLine(line); //sends the line to processLine if we want to check the character frequency
             }
-            if(status == 2){
-                checkPasswordStrength(line, filePath);
-                //System.out.println("checking line " + numLines);
+            if(status == 2 || status == 4){
+                checkPasswordStrength(line, curLineNum); //sends the line to checkPasswordStrength if we want to check the password's strength
             }
-            numLines++;
+            curLineNum++;
         }
         br.close();
         System.out.println("Finished reading the file.");
 
-        if (status == 0){
-            output(filePath);
-            status = 1;
-            System.out.println("First half done");
-            charCounts.clear();
-            stringReader(filePath);
-        }
-        if (status == 2) {
+        if (status == 1){
+            numLines = curLineNum;
+            output();
+            status = 2;
+            System.out.println("Finished generating character stats for first5.");
             csvPrinter.close();
+            stringReader();
+        }
+
+        if(status == 2){
             status = 3;
+            System.out.println("Finished checking password scores for first5.");
+            csvPrinter.close();
+            charCounts.clear();
+            stringReader();
+        }
+
+        if (status == 3){
+            output();
+            status = 4;
+            System.out.println("Finished generating character stats for last5.");
+            csvPrinter.close();
+            stringReader();
+        }
+
+        if(status == 4){
+            status = 5;
+            System.out.println("Finished checking password scores for last5.");
+            csvPrinter.close();
+            charCounts.clear();
         }
     }
 
@@ -80,7 +107,7 @@ public class CharStats {
         TreeMap<Integer, Integer> curCounts;
         Integer curCount;
 
-        if (status == 1)
+        if (status == 3)
             curLine = new StringBuilder(curLine).reverse().toString();
 
         for (int i = 0; i < length; i++) {
@@ -106,22 +133,20 @@ public class CharStats {
     }
 
 
-    public void output(String filePath) throws Exception{
+    public void output() throws Exception{
 
         //Initializes the FileWriter class, which sends an output file to the same directory with the same name appended by -Output.csv
-        if (status == 0) {
-            csvFilePath = filePath + "-Output.csv";
-            csvPrinter = new CSVPrinter(new FileWriter(csvFilePath), csvFormat);
-        }
+        csvFilePath = filePath + "-Output.csv";
+        csvPrinter = new CSVPrinter(new FileWriter(csvFilePath), csvFormat);
 
-        // print out counts
-        System.out.format("Printing results to: %s", csvFilePath);
+        // print out the location of the CSV
+        System.out.format("Printing character stats to: %s", csvFilePath);
         System.out.println();
 
         // print out the counts
-        if (status == 0)
+        if (status == 2)
             csvPrinter.printComment("Number of characters in each position:");
-        else
+        if (status == 4)
             csvPrinter.printComment("Number of characters (n) from end");
 
         char curChar;
@@ -142,7 +167,7 @@ public class CharStats {
                 }
 
                 // find the total count for this character and append to end of CSV record.
-                if (status == 1) {
+                if (status == 2) {
                     int totalCount = 0;
                     for (int val : curCounts.values()) {
                         totalCount += val;
@@ -156,9 +181,9 @@ public class CharStats {
         csvPrinter.flush();
 
         // print out percentages
-        if(status == 0)
+        if (status == 2)
             csvPrinter.printComment("Percent of passwords containing the character in each position:");
-        else
+        if (status == 4)
             csvPrinter.printComment("Percent of passwords containing the character in each position (n) from end:");
 
         for (int i = 32; i <= 126; i++) {
@@ -177,7 +202,7 @@ public class CharStats {
                 }
 
                 // find the total count for this character and append to end of CSV record.
-                if(status == 1) {
+                if(status == 2) {
                     int totalCount = 0;
                     for (int val : curCounts.values()) {
                         totalCount += val;
@@ -188,54 +213,49 @@ public class CharStats {
                     csvPrinter.println();
             }
         }
-        if(status == 1) {
-            csvPrinter.close();
-            System.out.println("Total lines processed: " + numLines);
-            status = 2;
-            stringReader(filePath);
-        }
     }
 
-    public void checkPasswordStrength (String curLine, String filePath) throws Exception{
+    public void checkPasswordStrength (String curLine, int curLineNum) throws Exception{
 
-        if(numLines == 0) {
+        if(curLineNum == 1) {
             csvFilePath = filePath + "-PasswordStrength.csv";
             csvPrinter = new CSVPrinter(new FileWriter(csvFilePath), csvFormat);
+            System.out.format("Printing password scores to: %s", csvFilePath);
+            System.out.println();
         }
 
-        boolean reverse = false;
-        int length = curLine.length();
+        if (status == 4)
+            curLine = new StringBuilder(curLine).reverse().toString();
+
         char curChar;
         TreeMap<Integer, Integer> curCounts;
         double linePct = 0;
 
-        if (reverse)
-            curLine = new StringBuilder(curLine).reverse().toString();
-
-        for (int i = 0; i < length; i++) {
-
-            linePct = 0;
+        for (int i = 0; i < 5; i++) {
             curChar = curLine.charAt(i);
             curCounts = (TreeMap<Integer, Integer>) charCounts.get(curChar);
 
-            for (int pos = 0; pos < 5; pos++) {
-                if (curCounts.containsKey(pos)) {
-                    double charPct = (curCounts.get(pos) / (double) numLines) * 100;
-                    charPct = 1/charPct;
-                    linePct += charPct;
-                }
+            double charPct = (curCounts.get(i) / (double) numLines) * 100;
+            charPct = 1/charPct;
+            linePct += charPct;
             }
-        }
-        csvPrinter.print("Line score for first 5 in line " + numLines + ": ");
+
+
+        if (status == 2)
+            csvPrinter.print("Line score for first 5 in line " + curLineNum + ": ");
+        if (status == 4)
+            csvPrinter.print("Line score for last 5 in line " + curLineNum + ": ");
+
+
+
         csvPrinter.print(linePct);
+        if (status == 4)
+            curLine = new StringBuilder(curLine).reverse().toString();
         csvPrinter.print(curLine);
         csvPrinter.println();
-        //System.out.println("Printing out line score");
+
         //check each character for percentage frequency in that position
 
         //take 1/that percentage and add it together with the other fractions so far.
-
-        //somehow output the line and its strength
-
     }
 }
